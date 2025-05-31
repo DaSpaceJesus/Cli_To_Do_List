068dc6f361c45db97cbd5f617b1ac2f8f772b9a7
Kasra Onsori <k_onsori@yahoo.com>
2025-05-31 21:32:52 +0330

    100% Done

--- main.py
+++ main.py
@@ -48,12 +48,6 @@ def list_tasks(tasks):
         for row in tasks:
             print("{:<0} , {:<6}, {:<5}, {:<5}".format(*row))
 
-
-def save_tasks(tasks):
-    with open(data_file, 'w') as f:
-        json.dump(tasks, f)
-        print("Tasks Saved")
-
 def mark_done(tasks):
     if len(tasks) <= 1:
         print("No tasks left to mark done!")
@@ -66,6 +60,44 @@ def mark_done(tasks):
     tasks[choice][3] = "Done"
     return tasks
 
+def edit_task(tasks):
+    print("\nWhich Task? :")
+    for index, task in enumerate(tasks[1:], start=1):
+        print(index, task)
+    choice = int(input("Enter your choice: "))
+    choice2 = int(input("What do you want to edit?\n 1. Name\n 2. Description\n 3. Due Date\n 4. Status\nYour choice: "))
+    if choice2 == 1:
+        tasks[choice][0] = input("Enter new task name: ")
+    elif choice2 == 2:
+        tasks[choice][1] = input("Enter new task description: ")
+    elif choice2 == 3:
+        tasks[choice][2] = input("Enter new task date: ")
+    elif choice2 == 4:
+        if tasks[choice][3] == "Not Done":
+            tasks[choice][3] = "Done"
+        elif tasks[choice][3] == "Done":
+            tasks[choice][3] = "Not Done"
+    return tasks
+
+def delete_task(tasks):
+    print("\nWhich Task? :")
+    for index, task in enumerate(tasks[1:], start=1):
+        print(index, task)
+    choice = int(input("Enter your choice: "))
+    tasks.pop(choice)
+    print("Tasks deleted!")
+    return tasks
+
+
+def save_tasks(tasks):
+    with open(data_file, 'w') as f:
+        json.dump(tasks, f)
+        print("Tasks Saved")
+
+def delete_all():
+    if os.path.exists(data_file):
+        os.remove(data_file)
+        print("Tasks Deleted")
 
 
 
@@ -74,25 +106,47 @@ def main():
     stay = True
     while stay:
         print("What do you want to do?\n 1. Add task\n 2. List all tasks\n 3. Mark a task as done")
-        choice1 = int(input(" 4. Edit an existing task\n 5. Delete an existing task\n 6. Exit\nYour Choice: "))
+        choice1 = int(input(" 4. Edit an existing task\n 5. Delete an existing task\n 6. Delete all!\n 7. Exit\nYour Choice: "))
         if choice1 == 1:
             add_task(tasks)
             save_tasks(tasks)
             Continue = int(input("Continue?\n 1. Yes\n 2. No\nYour choice: "))
             if Continue == 2:
                 stay = False
+
         elif choice1 == 2:
             list_tasks(tasks)
             Continue = int(input("Continue?\n 1. Yes\n 2. No\nYour choice: "))
             if Continue == 2:
                 stay = False
+
         elif choice1 == 3:
-            tasks= mark_done(tasks)
+            tasks = mark_done(tasks)
             save_tasks(tasks)
-            #Continue = int(input("Continue?\n 1. Yes\n 2. No\nYour choice: "))
-            #if Continue == 2:
-            stay = False
+            Continue = int(input("Continue?\n 1. Yes\n 2. No\nYour choice: "))
+            if Continue == 2:
+                stay = False
+
+        elif choice1 == 4:
+            tasks = edit_task(tasks)
+            save_tasks(tasks)
+            Continue = int(input("Continue?\n 1. Yes\n 2. No\nYour choice: "))
+            if Continue == 2:
+                stay = False
+
+        elif choice1 == 5:
+            delete_task(tasks)
+            save_tasks(tasks)
+            Continue = int(input("Continue?\n 1. Yes\n 2. No\nYour choice: "))
+            if Continue == 2:
+                stay = False
+
         elif choice1 == 6:
+            delete_all()
+            stay = False
+
+        elif choice1 == 7:
+            print("Bye!")
             stay = False
 
 
