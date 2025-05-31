17bcdc0aa2b7969602fb371b5a01034e21fe9bfc
Kasra Onsori <k_onsori@yahoo.com>
2025-05-31 19:56:37 +0330

    50% Done

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from datetime import datetime
 
 data_file = 'tasks.json'
 
+def load_data():
+    if os.path.exists(data_file):
+        with open(data_file, 'r') as f:
+            return json.load(f)
+    else:
+        tasks = [["Name", "Description", "Due Date", "Status"]]
+        return tasks
+
 def parse_date(date_str):
     formats = ["%Y/%m/%d", "%Y-%m-%d"]
     for format in formats:
@@ -13,37 +21,56 @@ def parse_date(date_str):
             continue
     raise ValueError(f"Date Format not recognized! Use one of {formats}")
 
-def load_data():
-    if os.path.exists(data_file):
-        with open(data_file, 'r') as f:
-            return json.load(f)
-    else:
-        tasks = []
-        return tasks
-
 def add_task(tasks):
-    taskitem = []
-    name = input("Enter task title: ")
-    taskitem.append(name)
-    description = input("Enter task description: ")
-    taskitem.append(description)
-    date = input("Enter task date(YYYY/MM/DD or YYYY-MM-DD): ")
-    parsed = parse_date(date)
-    taskitem.append(parsed)
-    tasks.append(taskitem)
+    multiple = True
+    while multiple:
+        taskitem = []
+        name = input("Enter task title: ")
+        taskitem.append(name)
+        description = input("Enter task description: ")
+        taskitem.append(description)
+        date = input("Enter task date(YYYY/MM/DD or YYYY-MM-DD): ")
+        parsed = parse_date(date)
+        taskitem.append(parsed.strftime("%Y/%m/%d"))
+        status = "Not Done"
+        taskitem.append(status)
+        tasks.append(taskitem)
+        choice = int(input("Task added! Do want to add another task?\n 1. Yes\n 2. No\nYour choice: "))
+        if choice == 2:
+            multiple = False
 
-def list_tasks():
-    none
+
+
+def list_tasks(tasks):
+    for row in tasks:
+        print("{:<5} , {:<10}, {:<15}, {:<20}".format(*row))
+
+def save_tasks(tasks):
+    with open(data_file, 'w') as f:
+        json.dump(tasks, f)
+        print("Tasks Saved")
 
 
 
 def main():
     tasks = load_data()
-    print("What do you want to do?\n 1. Add task\n 2. List all tasks\n 3. Mark a task as done")
-    choice1 = int(input(" 4. Edit an existing task\n 5. Delete an existing task\nYour Choice: "))
-    if choice1 == 1:
-        add_task(tasks)
-    print(tasks)
+    stay = True
+    while stay:
+        print("What do you want to do?\n 1. Add task\n 2. List all tasks\n 3. Mark a task as done")
+        choice1 = int(input(" 4. Edit an existing task\n 5. Delete an existing task\n 6. Exit\nYour Choice: "))
+        if choice1 == 1:
+            add_task(tasks)
+            save_tasks(tasks)
+            Continue = int(input("Continue?\n 1. Yes\n 2. No\nYour choice: "))
+            if Continue == 2:
+                stay = False
+        elif choice1 == 2:
+            list_tasks(tasks)
+            Continue = int(input("Continue?\n 1. Yes\n 2. No\nYour choice: "))
+            if Continue == 2:
+                stay = False
+        elif choice1 == 6:
+            stay = False
 
 
 
