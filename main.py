3bcd352ef2b4aeffdce3894e4071c4916eb8e52b
Kasra Onsori <k_onsori@yahoo.com>
2025-05-31 19:05:57 +0330

    added parse date and json data file

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
+import os
+import json
+from datetime import datetime
+
+data_file = 'tasks.json'
+
+def parse_date(date_str):
+    formats = ["%Y/%m/%d", "%Y-%m-%d"]
+    for format in formats:
+        try:
+            return datetime.strptime(date_str, format)
+        except ValueError:
+            continue
+    raise ValueError(f"Date Format not recognized! Use one of {formats}")
+
+def load_data():
+    if os.path.exists(data_file):
+        with open(data_file, 'r') as f:
+            return json.load(f)
+    else:
+        tasks = []
+        return tasks
+
 def add_task(tasks):
+    taskitem = []
     name = input("Enter task title: ")
-    tasks.append(name)
+    taskitem.append(name)
     description = input("Enter task description: ")
-    tasks.append(description)
-    date = input("Enter task date(YYYYMMDD): ")
-    tasks.append(date)
+    taskitem.append(description)
+    date = input("Enter task date(YYYY/MM/DD or YYYY-MM-DD): ")
+    parsed = parse_date(date)
+    taskitem.append(parsed)
+    tasks.append(taskitem)
+
+def list_tasks():
+    none
 
 
 
 def main():
-    tasks = []
+    tasks = load_data()
     print("What do you want to do?\n 1. Add task\n 2. List all tasks\n 3. Mark a task as done")
     choice1 = int(input(" 4. Edit an existing task\n 5. Delete an existing task\nYour Choice: "))
     if choice1 == 1:
